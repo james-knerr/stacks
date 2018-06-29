7a2f0249123d346b1fc6cb89e31a88cc66e10bf3
Knerr, James <jknerr@cardinalsolutions.com>
2018-06-29 10:17:30 -0400

    updates image modal

--- src/app/main.module/record-details.component/record-details.component.ts
+++ src/app/main.module/record-details.component/record-details.component.ts
@@ -9,14 +9,24 @@ import { RecordVM } from '../../shared.module/models/record-vm';
   })
   export class RecordDetailsComponent {
     public record: RecordVM;
+    public isImageVertical: boolean;
+    public isImageHorizontal: boolean;
 
     constructor(
         private _selfDialogRef: MatDialogRef<RecordDetailsComponent>) {}
 
-        public setModel(existingRecord?: RecordVM) {
-            this.record = existingRecord || new RecordVM();
+        public setModel(existingRecord: RecordVM) {
+            this.record = existingRecord;
+            const img = new Image();
+            img.src = this.record.imageUrl;
+            const width = img.naturalWidth;
+            const height = img.naturalHeight;
+            if (width >= height) {
+                this.isImageHorizontal = true;
+            } else {
+                this.isImageVertical = true;
+            }
         }
-
 public closeDetails() {
     this._selfDialogRef.close();
 }
